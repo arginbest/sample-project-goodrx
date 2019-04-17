a64231bc50d8a0938f8d1a23923dfb4470350306
Greg Saunders <waltisfrozen@gmail.com>
2019-04-16 22:57:18 -0700

    Updating build script to use default image name when not specified

--- build.sh
+++ build.sh
@@ -3,26 +3,27 @@
 if [ -z "$1" ]
 then
   echo -e "\nUsage: ./build.sh <path_to_Docker_image_on_public_registry>\n"
+  echo -e "Using default image waltisfrozen/sample-project-goodrx..."
+  IMAGE_NAME='waltisfrozen/sample-project-goodrx'
 else
   IMAGE_NAME=$1
-  echo $IMAGE_NAME
-
-  echo -e "\n##################################\n\nBuilding Docker image\n\n##################################\n"
-
-  docker build -t $IMAGE_NAME -f docker/Dockerfile docker/
-  echo -e "\n##################################\n\nPushing Docker image to public registry\n\n##################################\n"
-
-  docker push $IMAGE_NAME
-
-  echo -e "\n##################################\n\nCreating keypair for EC2 instances\n\n##################################\n"
-  ssh-keygen -t rsa -b 4096 -C "waltisfrozen@gmail.com" -P "" -f ./terraform/goodrx
-
-  echo -e "\n##################################\n\nInitializing Terraform\n\n##################################\n"
-
-  cd terraform
-  terraform init
-  terraform plan -var "image_name=$IMAGE_NAME"
-
-  echo -e "\n##################################\n\nApplying Terraform \n\n##################################\n"
-  terraform apply -var "image_name=$IMAGE_NAME" -auto-approve
 fi
+
+echo -e "\n##################################\n\nBuilding Docker image\n\n##################################\n"
+
+docker build -t $IMAGE_NAME -f docker/Dockerfile docker/
+echo -e "\n##################################\n\nPushing Docker image to public registry\n\n##################################\n"
+
+docker push $IMAGE_NAME
+
+echo -e "\n##################################\n\nCreating keypair for EC2 instances\n\n##################################\n"
+ssh-keygen -t rsa -b 4096 -C "waltisfrozen@gmail.com" -P "" -f ./terraform/goodrx
+
+echo -e "\n##################################\n\nInitializing Terraform\n\n##################################\n"
+
+cd terraform
+terraform init
+terraform plan -var "image_name=$IMAGE_NAME"
+
+echo -e "\n##################################\n\nApplying Terraform \n\n##################################\n"
+terraform apply -var "image_name=$IMAGE_NAME" -auto-approve
